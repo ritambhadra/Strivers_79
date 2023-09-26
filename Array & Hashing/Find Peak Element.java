92602f6f686be1754816c76284f44f34a26c127d
Ritam Bhadra <imritam29@gmail.com>
2023-09-27 01:07:45 +0530

    ..

---
+++ Array & Hashing/Find Peak Element.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+public class Solution {
+    public static int findPeakElement(ArrayList<Integer> arr) {
+        // Write your code here.
+        int n = arr.size();
+        if(n == 1) return 0;
+        if(arr.get(0) > arr.get(1)) return 0;
+        if(arr.get(n-1) > arr.get(n-2)) return n -1;
+        int low = 1 ;
+        int high = n -2;
+
+        while(low <= high){
+            int mid = low + (high -low)/2;
+            // If arr[mid] is the peak:
+            if (arr.get(mid - 1) < arr.get(mid) && 
+                arr.get(mid) > arr.get(mid + 1) )
+                return mid;
+
+            // If we are in the left:
+            if (arr.get(mid) > arr.get(mid - 1)) low = mid + 1;
+
+            // If we are in the right:
+            // Or, arr[mid] is a common point:
+            else high = mid - 1;
+        }
+        // Dummy return statement
+        return -1;
+    }
+};
+       
