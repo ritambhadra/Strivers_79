8dda3c3f9a478f4bdf45c2cfe42c9624f0c24425
Ritam Bhadra <imritam29@gmail.com>
2023-09-26 12:36:44 +0530

    modified merge sort

--- Array & Hashing/Number of Inversions.java
+++ Array & Hashing/Number of Inversions.java
@@ -31,3 +31,47 @@ public class Solution {
 }
 */
 
+// modified mersort for solving this qstn
+
+import java.util.*;
+
+public class Solution {
+    static int merge(int[] a, int low, int mid, int high){
+        int left = low;
+        int right = mid +1;
+        int count = 0;       //Modification: count variable to count the pairs:
+        ArrayList<Integer> ls = new ArrayList<>();
+        
+        while(left <= mid && right <= high){
+            if(a[left] <= a[right])
+                ls.add(a[left++]);
+            else{
+                count += (mid - left + 1);  //Modification
+                ls.add(a[right++]);
+            }
+        }
+        while(left <= mid)
+            ls.add(a[left++]);
+            
+        while(right <= high)
+            ls.add(a[right++]);
+
+        for(int i = low ; i <= high; i++)
+            a[i] = ls.get(i - low);
+
+        return count;     // Modification
+    }
+    static int mergesort(int[] a, int low, int high){
+        int count = 0;
+        if(low >= high) return count;
+        int mid = (low + high)/2;
+        count += mergesort(a, low, mid);
+        count += mergesort(a, mid+1, high);
+        count += merge(a, low, mid, high);
+        return count;
+    }
+    public static int numberOfInversions(int []a, int n) {
+        // Write your code here.
+        return mergesort(a, 0, n-1);
+    }
+}
